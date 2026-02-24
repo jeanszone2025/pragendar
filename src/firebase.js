f741aa009f6ffd7bbcbe5b97a24b73b3e632d39c
jeanszone2025 <jeanszone2025@gmail.com>
2026-02-23 22:44:36 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth"; // ADICIONADO
 
 const firebaseConfig = {
   apiKey: "AIzaSyCWFrcEYq6aL-J7dvL-u9poWywDO5WnIgA",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app); // ADICIONADO
